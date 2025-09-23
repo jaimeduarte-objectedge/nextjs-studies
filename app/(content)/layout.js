11a48b46415fe6cf5c15fc15619157b73a1f08b8
ja-carteiro <jaimead1706@gmail.com>
2025-09-22 23:38:36 -0300

    NEXTNEWS: Finished Section 4

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../globals.css";
 import MainHeader from "@/components/MainHeader/MainHeader.js";
 
 export const metadata = {
--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import { getNewsForYear } from "@/lib/news";
-import NewsList from "@/components/newsList/newslist";
-
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
-
-  return <NewsList news={news} />;
-}
