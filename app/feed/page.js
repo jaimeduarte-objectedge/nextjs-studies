fa5593172ab8ad8e27790d3aca81ac7518aa262f
ja-carteiro <jaimead1706@gmail.com>
2025-09-25 18:42:06 -0300

    NEXTPOSTS: Finished Section 8

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,22 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `Browse all our ${numberOfPosts} ${
+      numberOfPosts > 1 ? "posts" : "post"
+    }.`,
+    description: "Browse all our posts.",
+  };
+}
+
+// export const metadata = {
+//   title: 'Browse all our X posts.',
+//   description: 'Browse all our posts'
+// };
 
 export default async function FeedPage() {
   const posts = await getPosts();
