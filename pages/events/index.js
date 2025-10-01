b7a02038d193128102392091614b4fc818522da6
Jaime OE <jaime.duarte@objectedge.com>
2025-10-01 18:29:19 -0300

    Finished Section 12

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,23 @@
+import { Fragment } from "react/jsx-runtime";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
-    <div>
-      <EventList items={events}/>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
