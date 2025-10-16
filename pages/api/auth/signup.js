4c1a9d4d226952b4f4e64e9dbf52dd8b8de523e4
Jaime OE <jaime.duarte@objectedge.com>
2025-10-16 13:49:14 -0300

    Add simple signup with mongodb and bcryptjs

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,42 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db("auth-demo");
+
+    const existingUser = await db.collection("users").findOne({ email: email });
+
+    if (existingUser) {
+      res.status(422).json({ message: "User exists already!" });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created user!" });
+    client.close();
+  }
+}
